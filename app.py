f0bef8393e8fdcef610cd70d34e1e091882c801c
Thien (Andrew) Nguyen <nguyenhuuthien27296@gmail.com>
2022-06-04 19:27:39 +0200

    Add files

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+import pandas as pd
+from flask import Flask, request, render_template
+from sklearn import preprocessing
 import pickle
 
-# Create flask app
-flask_app = Flask(__name__)
-model = pickle.load(open("model.pkl", "rb"))
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+cols=['age','workclass','education','marital-status','occupation','relationship','race','gender','capital-gain','capital-loss',
+      'hours-per-week','native-country']
 
-@flask_app.route("/")
-def Home():
-    return render_template("index.html")
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@flask_app.route("/predict", methods = ["POST"])
+@app.route('/predict',methods=['POST'])
 def predict():
-    float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
-    prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+    feature_list = request.form.to_dict()
+    feature_list = list(feature_list.values())
+    feature_list = list(map(int, feature_list))
+    final_features = np.array(feature_list).reshape(1, 12) 
+    
+    prediction = model.predict(final_features)
+    output = int(prediction[0])
+    if output == 1:
+        text = ">50K"
+    else:
+        text = "<=50K"
+
+    return render_template('index.html', prediction_text='Employee Income is {}'.format(text))
+
 
 if __name__ == "__main__":
-    flask_app.run(debug=True)
+    app.run(debug=True)
